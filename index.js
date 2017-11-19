1136a5c6820f45886ecd7bbfac7f40ae222037a8
Jennifer Haddock <jentx123@hotmail.com>
2017-11-19 05:21:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,12 +6,11 @@ function logDriverNames(array){
 }
 
 function logDriversByHometown(array, location){
-  array.forEach(function(findMatching){
-    return array.filter(function(matchIt){
-      return matchIt.name === location
-    })
-    console.log(element.name)
-  });
+  array.forEach((element) => {
+    if (element.hometown === location){
+       console.log(element.name);
+    }
+  })
 }
 
 
