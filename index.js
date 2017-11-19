0c18016c48c7b016b4f71d334f6bcc1609109f95
Jennifer Haddock <jentx123@hotmail.com>
2017-11-19 05:14:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ function logDriversByHometown(array, location){
 
 
 function driversByRevenue(array){
-
+  array.sort(function (a, b){
+    return a.revenue - b.revenue;
+  })
 }
 
 function driversByName(array){
